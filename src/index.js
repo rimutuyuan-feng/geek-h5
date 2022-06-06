30a34897c2603234db7aa4058915576388105120
riddle <482825911@qq.com>
2022-06-06 17:54:21 +0800

    feat: 基本store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'App'
-import './assets/styles/index.scss'
+import App from '@/App'
+import '@scss/index.scss'
+import { Provider } from 'react-redux'
+import store from './store'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
---
+++ src/store/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+const rootReducer = combineReducers({
+	test: (state = 0, action) => state,
+})
+export default rootReducer
