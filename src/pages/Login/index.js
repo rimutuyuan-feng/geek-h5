c6eb476a456c7a1b61076cc4dcf74e398842fa36
riddle <482825911@qq.com>
2022-06-12 23:41:50 +0800

    feat: 登录功能—登录表单完成

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,76 @@
 import React from 'react'
-
+import NavBar from '@/components/NavBar'
+import Input from '@/components/Input'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import styles from './index.module.scss'
 export default function Login() {
-	return <div>这里是登录</div>
+	const formik = useFormik({
+		initialValues: {
+			mobile: '',
+			code: '',
+		},
+		onSubmit: (values) => {
+			console.log(values)
+		},
+		validationSchema: Yup.object({
+			mobile: Yup.string()
+				.required('手机号不能为空')
+				.matches(/^1[3-9]\d{9}$/, '手机号格式错误'),
+			code: Yup.string()
+				.required('验证码不能为空')
+				.matches(/^\d{6}$/, '验证码格式错误'),
+		}),
+	})
+	const {
+		values: { mobile, code },
+		handleSubmit,
+		handleChange,
+		errors,
+		handleBlur,
+		touched,
+	} = formik
+	return (
+		<div className={styles.root}>
+			<NavBar>登录</NavBar>
+			<div className='content'>
+				<h3>登录</h3>
+				<form onSubmit={handleSubmit}>
+					<div className='input-item'>
+						<Input
+							name='mobile'
+							placeholder='请输入手机号'
+							value={mobile}
+							onChange={handleChange}
+							onBlur={handleBlur}
+						/>
+						{touched.mobile && errors.mobile && (
+							<div className='validate'>{errors.mobile}</div>
+						)}
+					</div>
+					<div className='input-item'>
+						<div className='input-box'>
+							<Input
+								value={code}
+								name='code'
+								placeholder='请输入验证码'
+								extra='获取验证码'
+								onExtraClick={() => {
+									console.log('点击了')
+								}}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+						</div>
+						{touched.code && errors.code && (
+							<div className='validate'>{errors.code}</div>
+						)}
+					</div>
+					<button type='submit' className='login-btn'>
+						登录
+					</button>
+				</form>
+			</div>
+		</div>
+	)
 }
