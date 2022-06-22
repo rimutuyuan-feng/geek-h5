4b096c88b1fa6a23e354a0289aeea49804fce09b
riddle <482825911@qq.com>
2022-06-22 22:25:25 +0800

    chore: 统一为请求头配置token信息

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,10 +1,15 @@
 import { Toast } from 'antd-mobile'
 import axios from 'axios'
+import { getTokenInfo } from './storage'
 const http = axios.create({
 	baseURL: 'http://toutiao.itheima.net/v1_0',
 	timeout: 3000,
 })
 http.interceptors.request.use((config) => {
+	const token = getTokenInfo().token
+	if (token) {
+		config.heards['Authorization'] = 'Bearer ' + token
+	}
 	return config
 })
 http.interceptors.response.use(
@@ -12,11 +17,12 @@ http.interceptors.response.use(
 		return response.data
 	},
 	(error) => {
-        if(error.response){
-            Toast.show({content:'服务器繁忙'})
-        }else{
-            Toast.show({content:error.response.data.message})
-        }
-        Promise.reject(error)}
+		if (error.response) {
+			Toast.show({ content: '服务器繁忙' })
+		} else {
+			Toast.show({ content: error.response.data.message })
+		}
+		Promise.reject(error)
+	}
 )
 export default http
