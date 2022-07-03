8d299dc2fc3362d743e398bee935704198a3361b
riddle <482825911@qq.com>
2022-07-03 15:52:03 +0800

    chore: 配置一级路由

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Suspense } from 'react'
 import { Redirect } from 'react-router-dom'
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const Home = React.lazy(() => import('@/pages/Home'))
+const Layout = React.lazy(() => import('@/pages/Layout'))
 const Login = React.lazy(() => import('@/pages/Login'))
+const Edit = React.lazy(() => import('@/pages/Edit'))
 const App = () => {
 	return (
 		<Router>
 			<div className='app'>
-				<Link to='/login'>登录</Link>
-				<Link to='/home'>首页</Link>
-				<Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
 					<Switch>
 						<Redirect path='/' exact to={'/home'} />
 						<Route path={'/login'} component={Login} />
-						<Route path={'/home'} component={Home} />
+						<Route path={'/home'} component={Layout} />
+						<Route path={'/profile/edit'} component={Edit} />
 					</Switch>
 				</Suspense>
 			</div>
